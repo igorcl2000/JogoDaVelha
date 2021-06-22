ff4f65cf9172229e929c7d8aa91b7b8aa9ae92e7
Igor Costa de Lima <igorcl2000@gmail.com>
2021-06-22 13:43:43 -0300

    CorreçãoBot

--- JogoDaVelha/src/Computador.java
+++ JogoDaVelha/src/Computador.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Computador extends Jogador {
 
     public Computador(int jogador) {
@@ -7,11 +9,29 @@ public class Computador extends Jogador {
 
     @Override
     public void jogar(Tabuleiro tabuleiro) {
-
+        Tentativa(tabuleiro);
+        tabuleiro.setPosicao(tentativa, jogador);
     }
 
     @Override
     public void Tentativa(Tabuleiro tabuleiro) {
+        Random numeroRandomico = new Random();
+        do {
+            do {
+                numeroRandomico.setSeed(numeroRandomico.nextInt(9999999));
+                tentativa[0] = numeroRandomico.nextInt(3) + 1;
 
+            } while (tentativa[0] > 3 || tentativa[0] < 1);
+
+            do {
+                numeroRandomico.setSeed(numeroRandomico.nextInt(9999999));
+                tentativa[1] = numeroRandomico.nextInt(3) + 1;
+
+            } while (tentativa[1] > 3 || tentativa[1] < 1);
+
+            tentativa[0]--;
+            tentativa[1]--;
+
+        } while (!checaTentativa(tentativa, tabuleiro));
     }
 }
